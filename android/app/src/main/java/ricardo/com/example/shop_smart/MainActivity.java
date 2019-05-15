5de0fb527e2518e350c2a749b8f062333b95f935
1355115 <1355115@students.wits.ac.za>
2019-05-15 16:58:17 +0200

    Readme file

---
+++ android/app/src/main/java/ricardo/com/example/shop_smart/MainActivity.java
@@ -0,0 +1,13 @@
+package ricardo.com.example.shop_smart;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
